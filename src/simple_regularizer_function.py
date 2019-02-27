df9e74a3bf1272e1ff16433ac60d1b8594d89788
rmarcacini <40037976+rmarcacini@users.noreply.github.com>
2019-02-27 13:34:47 -0300

    Create simple_regularizer_function.py

---
+++ src/simple_regularizer_function.py
@@ -0,0 +1,81 @@
+import network
+import regularization
+import numpy as np
+
+
+class SimpleRegularizerFunction:
+    
+    def __init__(self,max_iterations,min_e):
+        self.max_iterations=max_iterations
+        self.min_e=min_e
+    
+    def propagation(self,net):
+        
+        iteration = 0
+        dim = -1
+        
+        while True:
+        
+            nodes = net.getNodes()
+            for node in nodes:
+                if(net.getNodeData(node,'labeled')==True): continue
+                f = net.getNodeData(node,'f')
+                dim = len(f)
+                f_temp = np.array([0]*dim)
+                neighbors = net.getNeighbors(node)
+
+                normalization = 0
+                for neighbor in neighbors:
+                    penalty = net.getDegree(neighbor)
+                    if(net.getNodeData(neighbor,'labeled')==True): penalty=1
+                    normalization += 1/penalty
+                    f_neighbor = net.getNodeData(neighbor,'f')
+                    f_temp = f_temp + (f_neighbor/penalty)
+
+                f_temp = f_temp/normalization
+                net.setNodeData(node,'f_temp',f_temp)
+
+            e = 0
+            for node in nodes:
+                if(net.getNodeData(node,'labeled')==True): continue
+                f = net.getNodeData(node,'f')
+                f_temp = net.getNodeData(node,'f_temp')
+                e += np.sum(np.square(f - f_temp))
+                net.setNodeData(node,'f',f_temp)
+                f = None
+        
+            iteration += 1
+            print("iteration="+str(iteration)+"\te="+str(e))
+            
+            if iteration >= self.max_iterations: break
+            if e < self.min_e: break
+        
+        
+        for node in nodes:
+            if(net.getNodeData(node,'labeled')==True):
+                f_temp = np.array([0]*dim)
+                neighbors = net.getNeighbors(node)
+                normalization = 0
+                for neighbor in neighbors:
+                    penalty = net.getDegree(neighbor)
+                    if(net.getNodeData(neighbor,'labeled')==True): penalty=1
+                    normalization += 1/penalty
+                    f_neighbor = net.getNodeData(neighbor,'f')
+                    f_temp = f_temp + (f_neighbor/penalty)
+                f_temp = f_temp/normalization
+                net.setNodeData(node,'f_temp',f_temp)
+
+        for node in nodes:
+            if(net.getNodeData(node,'labeled')==True):
+                net.setNodeData(node,'f',net.getNodeData(node,'f_temp'))
+                
+                
+net = Network()
+net.setMemoryCache(True)
+net.loadCSV('network-acm.csv'')
+
+srf = SimpleRegularizerFunction(30,0.05)
+reg = Regularization(net,'labels-acm.csv')
+reg.propagate(srf.propagation)
+
+net.printNodes()
